168967c32f62baec5a4e7ada244790763d737e8e
Anuj Kumar <anuj846k@gmail.com>
2024-06-27 19:47:51 +0530

    VideoBackground Ui imrpovement & autoplayYT

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,16 +1,19 @@
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="pt-[28%] px-12 absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
       <h1 className="text-7xl font-bold ">{title}</h1>
       <p className="py-5 text-xl w-1/2">{overview}</p>
       <div className="flex items-center">
-        <button className="px-10 items-center flex p-4 rounded-xl  border text-black bg-white text-5xl hover:bg-black hover:text-white transition-all duration-500">
-          <img src="./play.svg" className="h-14 w-14 mr-4" alt="" />
-           Play
+        <button className="px-7 items-center flex py-4 rounded-xl  border text-black bg-white text-4xl hover:bg-black hover:text-white transition-all duration-500">
+          <img src="./play.svg" className="h-10 w-10 mr-4" alt="" />
+          Play
         </button>
 
-        <button className="px-12 py-5 rounded-xl m-4 ml-5 border text-white bg-gray-600 text-5xl">
-          Details
+        <button className="px-4 items-center flex  py-4 rounded-xl m-4 ml-5 border text-white bg-gray-600 text-4xl">
+          <AiOutlineInfoCircle className="h-10 w-10 mr-4" />
+          More Info
         </button>
       </div>
     </div>
