7ac54e62d97b1420b82ea362d0f35eb714199e50
anuj846k <anuj846k@gmail.com>
2024-07-03 23:39:24 +0530

    removed

---
+++ src/utils/constant.js
@@ -0,0 +1,29 @@
+export const User_Avatar =
+  "https://avatars.githubusercontent.com/u/136173245?v=4";
+//change it later
+
+export const Api_options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGI2MWQxZmYyZDVmOGQ2NWJmYzc3N2QzM2Q2M2FlYSIsIm5iZiI6MTcxOTMyNTY3Ni40ODM0NDYsInN1YiI6IjY2N2FkMzE2ZmQ3MmNjZmRjZTVhMzU0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uRfsnPF-F4GbQb-dw6DF1-dOVjKGash7YPLgkGwRgEQ",
+  },
+};
+
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGI2MWQxZmYyZDVmOGQ2NWJmYzc3N2QzM2Q2M2FlYSIsIm5iZiI6MTcxOTMyNTY3Ni40ODM0NDYsInN1YiI6IjY2N2FkMzE2ZmQ3MmNjZmRjZTVhMzU0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uRfsnPF-F4GbQb-dw6DF1-dOVjKGash7YPLgkGwRgEQ
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const BG_IMG_CDN_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+
+export const SUPPORTED_LANGAUGES = [
+  { identifers: "en", name: "English" },
+  { identifers: "hn", name: "Hindi" },
+  { identifers: "sp", name: "Spanish" },
+];
+
+
+
+// export const OPENAI_KEY ="openai key here";
