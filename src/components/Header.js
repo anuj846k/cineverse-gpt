8d7320ebd5c70d278ac989831bc0e35c64427186
Anuj Kumar <[anuj846k@gmail.com]>
2024-07-08 12:43:13 +0530

    Responsiveness of first half completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
 import { toggleGptSearchView } from "../utils/gptSlice";
-import { SUPPORTED_LANGAUGES } from "../utils/constant";
+import { SUPPORTED_LANGUAGES } from "../utils/constant";
 import { changeLanguage } from "../utils/configSlice";
+import { TbLogout2 } from "react-icons/tb";
+
 
 const Header = () => {
   const navigate = useNavigate();
@@ -58,43 +60,46 @@ const Header = () => {
     dispatch(changeLanguage(e.target.value));
   };
 
-
-
   return (
-    <div className="absolute w-full md:px-8 md:py-2 px-2 py-0 bg-gradient-to-b from-black z-10 flex md:flex-row justify-between flex-col sm: ">
-      <img className="w-80 py-0 -mt-2 md:mb-2 mx-auto md:mx-0 " src="./download.png" alt="Logo" />
+    <div className="absolute w-full bg-gradient-to-b from-black z-10 flex flex-col md:flex-row md:px-8 md:py-2 px-2 py-0 items-center justify-between">
+      <img
+        className="md:w-80 w-44 py-0 mb-2 md:mb-0 mx-auto md:mx-0"
+        src="./download.png"
+        alt="Logo"
+      />
       {user && (
-        <div className="flex items-center mb-10 -mt-7 flex-row text-xs ">
+        <div className="flex items-center flex-row text-xs bg-blue-950 rounded-full md:rounded-full px-12  py-1  md:space-x-4 space-x-1 space-y-0 -mt-10 md:-mt-0 md:mb-10">
           {showGptSearch && (
             <select
-              className="py-3 px-2 rounded border-none bg-gray-900 text-white m-2"
+              className="py-2 px-3 rounded bg-gray-900 text-white border-none -ml-9 md:ml-0"
               onChange={handleLanguageChange}
             >
-              {SUPPORTED_LANGAUGES.map((lang) => (
+              {SUPPORTED_LANGUAGES.map((lang) => (
                 <option key={lang.identifers} value={lang.identifers}>
                   {lang.name}
                 </option>
               ))}
-              
             </select>
           )}
           <button
             onClick={handleGptSearch}
-            className="p-2 rounded-lg bg-orange-200 font-bold mx-4 m-2"
+            className="py-2 px-3 rounded-md bg-orange-200 font-bold "
           >
-           {showGptSearch ? "Homepage" : "GPT Search"}
+            {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
-          <img
-            className="w-16 h-16 mr-3 rounded-full"
-            src={user?.photoURL}
-            alt=""
-          />
 
+          
+          <img
+            className="md:w-16 md:h-16 w-12 h-12 rounded-full"
+            style={{ marginLeft: "30px" }}
+            src={user?.photoURL}
+            alt="User profile"
+          />
           <button
             onClick={handleSignOut}
-            className="bg-orange-500 font-bold text-white md:p-2 rounded-full md:rounded-md border-1 border-black p-1"
+            className="bg-orange-500 font-bold text-white p-2 rounded-md "
           >
-            Sign out
+            <TbLogout2 />
           </button>
         </div>
       )}
